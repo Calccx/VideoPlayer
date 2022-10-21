f67c09900acdc2f85ce7b7ce5820ad870276a950
Calccx <allcalc@outlook.com>
2022-10-21 21:47:43 +0800

    利用css、js更新播放器

--- button.js
+++ button.js
@@ -1,8 +1,6 @@
 var video;
-var display;
 window.onload = function () {
     video = document.getElementById("videoPlayer");
-    display = document.getElementById("displayStatus");
 }
 // 定义播放按钮
 function play() {
@@ -16,12 +14,6 @@ function pause() {
 function stop() {
     video.pause();
     video.currentTime = 0;
-}
-// 设置进度条
-function progressUpdate() {
-    var positionBar = document.getElementById("positionBar");
-    positionBar.style.width = (video.currentTime / video.duration * 100) + "&nbsp";
-    display.innerHTML = (Math.round(video.currentTime * 100) / 100) + "s";
 }
 function VideoInput() {
     var file = document.getElementById('file').files[0];
